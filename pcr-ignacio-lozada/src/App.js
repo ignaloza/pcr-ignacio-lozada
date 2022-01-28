f3e5cc677ce15f3468afc6d38514d9a709b50471
IgnacLozada <ignacio_lozada@hotmail.com>
2022-01-28 03:16:35 +0100

    initial commit

---
+++ pcr-ignacio-lozada/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import medio from "./medio.PNG";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src ={medio} alt="medio"></img>
+    </div>
+  );
+}
+
+export default App;
